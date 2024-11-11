0c8373d59ceec0fc053ca91032be5bd1546d6a41
yameen65 <yameenaziz607@gmail.com>
2024-11-11 15:44:54 +0500

    title change

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,9 +5,10 @@
 
     @foreach($posts as $post)
         <div class="post-preview mt-4">
-            <h2><a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a></h2>
+            <h2><a href="{{ route('posts.show', $post) }}"></a></h2>
+            <h3>{{ $post->tittle }}</h3>
             <p>{{ Str::limit($post->content, 100) }}</p>
-            
+
             <a href="{{ route('posts.show', $post) }}" class="btn btn-link">Read More</a>
         </div>
     @endforeach
