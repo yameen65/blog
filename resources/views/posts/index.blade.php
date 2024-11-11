d66a71177566799b0600f388b7a9509fd4be6cfe
yameen65 <yameenaziz607@gmail.com>
2024-11-11 16:16:38 +0500

    git pull

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,9 +6,9 @@
     @foreach($posts as $post)
         <div class="post-preview mt-4">
             <h2><a href="{{ route('posts.show', $post) }}"></a></h2>
-            <h3>{{ $post->tittle }}</h3>
+            <h1>{{ $post->tittle }}</h1>
             <p>{{ Str::limit($post->content, 100) }}</p>
-
+            
             <a href="{{ route('posts.show', $post) }}" class="btn btn-link">Read More</a>
         </div>
     @endforeach
