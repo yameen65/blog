bf1df3219996df4808204a38dd6c4f94098fc39e
yameen65 <yameenaziz607@gmail.com>
2024-11-11 16:55:43 +0500

    git pull

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Blog</title>
+    <title>Laravel Blog</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <h1>Simple Blog</h1>
+        <h1>Laravel Blog</h1>
         @yield('content')
     </div>
 </body>
